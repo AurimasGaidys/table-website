38908fb98a69a122de6cc56516a542e39fdfdc89
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2024-02-13 22:17:12 +0200

    Draw a circle.

--- aurimas-table/components/table/table.tsx
+++ aurimas-table/components/table/table.tsx
@@ -1,7 +1,25 @@
-import React, { useRef } from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 
-const Canvas = (props: any) => {
-  return <canvas {...props} />;
+const Table = (props: any) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const draw = (ctx: any) => {
+      ctx.fillStyle = "#000000";
+      ctx.beginPath();
+      ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+      ctx.fill();
+    };
+
+    const context = canvasRef?.current?.getContext("2d");
+    if (context) {
+      // draw samething.
+      draw(context);
+    }
+  }, []);
+
+  return <canvas ref={canvasRef} {...props} />;
 };
 
-export default Canvas;
+export default Table;
