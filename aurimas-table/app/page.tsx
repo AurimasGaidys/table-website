5e046e59c5e61fef64b0d4562f857c19b7a30c93
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2024-02-16 17:17:23 +0200

    Add image on canvas

--- aurimas-table/app/page.tsx
+++ aurimas-table/app/page.tsx
@@ -1,11 +1,42 @@
+"use client";
+
 import Table from "@/components/table/table";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+  const yPosAnim = useTransform(
+    scrollYProgress,
+    [0, 0.4, 1],
+    [100, -1500, -1500]
+  );
+  const xPosAnim = useTransform(
+    scrollYProgress,
+    [0, 0.1, 0.3, 1],
+    [100, 100, -1500, -1500]
+  );
+
+  const rotAnim = useTransform(
+    scrollYProgress,
+    [0, 0.1, 0.11, 0.3, 0.31, 1],
+    [0, 0, -15, -15, 0, 0]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-[9999px] flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex bg-[#333333]">
-        <div>A table</div>
-        <Table />
+        <motion.div
+          style={{
+            position: "fixed",
+            top: yPosAnim,
+            x: xPosAnim,
+            rotate: rotAnim,
+          }}
+          transition={{ type: "spring" }}
+        >
+          <Table />
+        </motion.div>
+        <div></div>
       </div>
     </main>
   );
--- aurimas-table/components/table/table.tsx
+++ aurimas-table/components/table/table.tsx
@@ -1,25 +1,72 @@
 "use client";
 import React, { useEffect, useRef } from "react";
+import Image from "next/image";
 
 const Table = (props: any) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const planPositions = [
+    { x: 0, y: 0 },
+    { x: 0, y: 85 },
+    { x: 0, y: 140 },
+    { x: 0, y: 210 },
+    { x: 0, y: 280 },
+    { x: 310, y: 50 },
+    { x: 320, y: 100 },
+  ];
+
+  for (let i = 0; i < 30; i++) {
+    for (let j = 0; j < 30; j++) {
+      planPositions.push({ x: 320 * i, y: 85 * j });
+    }
+  }
 
   useEffect(() => {
+    const canvas = canvasRef?.current;
+    const context = canvas?.getContext("2d");
+
     const draw = (ctx: any) => {
       ctx.fillStyle = "#000000";
       ctx.beginPath();
       ctx.arc(50, 100, 20, 0, 2 * Math.PI);
       ctx.fill();
+      ctx.strokeStyle = "red";
+      ctx.strokeRect(0, 0, canvas?.width, canvas?.height);
     };
 
-    const context = canvasRef?.current?.getContext("2d");
-    if (context) {
+    if (canvas && context) {
+      canvas.width = 3000;
+      canvas.height = 3000;
+
       // draw samething.
       draw(context);
+
+      // Get the `img` from reference
+      const image = imgRef.current;
+
+      if (image === null) return;
+
+      // Draw the image to the context
+      planPositions.map((x) => {
+        context.drawImage(image, x.x, x.y);
+      });
     }
   }, []);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return (
+    <>
+      <canvas ref={canvasRef} {...props} />
+      <Image
+        ref={imgRef}
+        src="/plank.jpg"
+        alt="Next.js Logo"
+        width={200}
+        height={20}
+        priority
+        draggable="false"
+      />
+    </>
+  );
 };
 
 export default Table;
